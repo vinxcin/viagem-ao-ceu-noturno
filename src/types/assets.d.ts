cc3fc574f258784ed8f9ae7d978962a1a81f13a1
VinxciusRosa <v.rosa@aluno.ifsp.edu.br>
2025-06-02 12:59:38 -0300

    atualização build

---
+++ src/types/assets.d.ts
@@ -0,0 +1,9 @@
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.gif';
+declare module '*.svg';
+declare module '*.webm';
+declare module '*.mp3';
+declare module '*.wav';
+declare module '*.ogg';
